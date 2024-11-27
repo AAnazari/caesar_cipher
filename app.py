4daea5c3161cf3ccc8d4166eb13cc6d27abf2887
Ali Ahmad <aliahmadnazari13@gmail.com>
2024-11-27 14:16:54 +0100

    Creating the index.HTML file for End User and coding the HTML Tags. Also this file is rendered.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 
 @app.route('/')
+def index():
+    return render_template("index.html")
+'''
 def hello():
     return '<h1>Caesar Cipher</h1>'
+'''
 
 @app.route('/about/')
 def about():
