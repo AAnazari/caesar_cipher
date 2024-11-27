f8a2388d36144cedabe0bda7a4251b8d10021954
Ali Ahmad <aliahmadnazari13@gmail.com>
2024-11-27 14:46:04 +0100

    Implementing the GET and POST request handling to process Caesar cipher encryption. Extracting 'text' and 'shift' values from the form, and applying the Caesar cipher algorithm and render result on the page.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+# Importing the Cipher function to encrypt the message
+from caesar_cipher.cipher import caesar
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        text = request.form["text"]
+        shift = int(request.form["shift"])
+
+        try:
+            result = caesar(text, shift)
+            return render_template("index.html", result=result)
+
+        except ValueError as e:
+            return render_template("index.html", error=str(e))
+
     return render_template("index.html")
 '''
 def hello():
